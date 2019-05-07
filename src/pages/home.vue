01c72c2b5ab7bc7c49c505bc41a7492910704eea
pdhoward <patrick.howard@hotmail.com>
2019-05-07 14:50:45 -0400

    fixed repeatable element for images

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,58 +1,51 @@
 
 <template lang="html">
-   <div style="height: 400px">
+<div class="home">
+  <a href="https://github.com/pdhoward/gallery" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
+  <div class="logo">
+    <img src="../assets/logo.png" />
+  </div>
+  <h3 class="title">Gallery: Connections</h3>
+   <div class="previews">
      <div :key="key" v-for="(img, key) in images" >
           
             <div class="preview">
                 <router-link v-bind:to="'/resume/pwasample'">
                     <div class="preview-wrapper">
-                         <img :src="imageDir + key.substr(1)">  
+                         <img :src="imageDir + img">  
                         <span>Mobile Retail</span>
                     </div>
                 </router-link>
             </div>
-
      </div>
     </div>
- </template>
+</div>
+</template>
 <script>
 import Vue from 'vue';
-
-//let arr = require.context("../assets/preview", true, /\.png$/)
 
 let imagesObj
 function requireAll(r) {
       var imgs = {}
-      r.keys().forEach(key => (imgs[key] = r(key)))
-      imagesObj = imgs
-      console.log(imgs)
+      r.keys().forEach(key => {
+          imgs[key] = r(key)         
+        })
+      imagesObj = imgs;      
     }
-requireAll(require.context('@/assets/preview', true, /\.png$/));
 
+requireAll(require.context('@/assets/preview', true, /\.png$/));
 
 export default Vue.component('resume', {
     name: 'app',
-  data() {
-    return {
-      imageDir: "src/assets/preview", // you know this already just from directory structure
-      images: imagesObj
-    }
-  },
-    /*
-  mounted() {
-    this.importAll(require.context(this.imageDir, true, /\.png$/))
-  },
-  methods: {
-    importAll(r) {
-      var imgs = {}
-      r.keys().forEach(key => (imgs[key] = r(key)))
-      this.images = imgs
-    }
+    data() {
+        return {
+        imageDir: "", // reference the root, because webpack places imgaes in static directory
+        images: imagesObj
+        }
   }
-  */
+   
 });
 </script>
-
 
 <style scoped>
 .home {
